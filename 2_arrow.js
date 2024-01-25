d63c2b110295c49f89b0fe3cf8b940537a35cb07
Juan Pablo M. <pablochileidh@gmail.com>
2024-01-25 19:45:02 -0300

    ajustes

--- 2_arrow.js
+++ 2_arrow.js
@@ -1,9 +1,9 @@
 //Transforma la siguiente función a una arrow function de una línea.
-
 let suma = function(a, b){
     return a + b
     }
     
-    //respuesta
-
+//respuesta
 let sumaArrow = (a, b) => a + b;
+
+console.log(sumaArrow(2,2))
