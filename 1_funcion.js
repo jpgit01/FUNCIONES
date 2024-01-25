41457ced8d223ced964fd0cd335d5a76062f9500
Juan Pablo M. <pablochileidh@gmail.com>
2024-01-25 19:37:55 -0300

    inicio de proyecto

---
+++ 1_funcion.js
@@ -0,0 +1,10 @@
+//Transforma esta declaración de función a una función de expresión
+function example(a, b, c){
+    return a+b+c
+    }
+
+//respuesta
+
+let exampleExpresion = function(a, b, c){
+    return a+b+c
+}
---
+++ 2_arrow.js
@@ -0,0 +1,6 @@
+//Transforma la siguiente función a una arrow function de una línea.
+let suma = function(a, b){
+    return a + b
+    }
+    
+    //respuesta
