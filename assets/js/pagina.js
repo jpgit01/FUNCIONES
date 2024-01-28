8d2babc11775737329efdd6718cb9bbb299f57c1
Juan Pablo M. <pablochileidh@gmail.com>
2024-01-27 23:36:02 -0300

    avanzando en ejercicio

--- assets/js/pagina.js
+++ assets/js/pagina.js
@@ -1,26 +1,53 @@
-document.body.onload = addElement;
+document.body.onload = addElement('white');
+
+
+//creacion del primer div para iniciar el ejercicio con las letras a, s y d
 function addElement() {
-  var newDiv = document.createElement("div");
-  newDiv.setAttribute("id", "key");
-  document.body.appendChild(newDiv);
-  let key =document.getElementById('key').setAttribute("style","width:200px; height: 200px; background-color: white; border: 5px solid black");
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "key");
+    document.body.appendChild(newDiv);
+    let key = document.getElementById("key").setAttribute("style","width:200px; height: 200px; backgroundColor: white; border: 5px solid black");
 }
 
-let color= '';
+//creacion de la funcion para agregar div a las teclas q, w, e
+function addElementSegundo() {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "keySegundo");
+    document.body.appendChild(newDiv);
+    let keySegundo = document.getElementById("keySegundo").setAttribute("style","width:200px; height: 200px; backgroundColor: white; border: 5px solid black");
+}
 
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'a') {
+
+
+let color = "";
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "a") {
     /* Cambiar a color 1 */
-    color = 'pink';
+    color = "pink";
     key.style.backgroundColor = color;
-    } else if (event.key === 's') {
+  } else if (event.key === "s") {
     /* Cambiar a color 2 */
-    color = 'orange';
+    color = "orange";
     key.style.backgroundColor = color;
-    } else if (event.key === 'd') {
-        /* Cambiar a color 3 */
-        color = 'LightCyan';
-        key.style.backgroundColor = color;
-        }
-    })
+  } else if (event.key === "d") {
+    /* Cambiar a color 3 */
+    color = "LightCyan";
+    key.style.backgroundColor = color;
+
+//creacion de nuevos divs
     
+  } else if (event.key === "q") {
+    /* Crear 4 */
+    color = "Purple";
+    key.style.backgroundColor = color;
+  } else if (event.key === "w") {
+    /* Crear 5 */
+    color = "grey";
+    key.style.backgroundColor = color;
+  } else if (event.key === "e") {
+    /* Crear 6 */
+    color = "DarkSalmon";
+    key.style.backgroundColor = color;
+  }
+});
